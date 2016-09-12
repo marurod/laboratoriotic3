1fa39d2717af955840e3eba4a7f3928745776673
marurod <maaaru-17@live.com>
2016-09-12 19:23:22 -0300

    Add files via upload

--- laboratoriotic3/modulo4/easier/SpeakGoodBye.js
+++ laboratoriotic3/modulo4/easier/SpeakGoodBye.js
@@ -11,14 +11,13 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var byeSpeaker= {};
-byeSpeaker.speakWord = "Good Bye";
+var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-byeSpeaker.speak = function(name) {
-  console.log(byeSpeaker.speakWord + " " + name);
+function speak(name) {
+  console.log(speakWord + " " + name);
 }
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
